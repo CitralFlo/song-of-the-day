2748189938b3735bf226b3293416ea12260577bf
citralflo <wojtas.michal90@gmail.com>
2024-03-30 21:26:00 +0100

    Initial commit

---
+++ src/main/java/dev/citralflo/songoftheday/SongOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package dev.citralflo.songoftheday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongOfTheDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/citralflo/songoftheday/SongOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.citralflo.songoftheday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
